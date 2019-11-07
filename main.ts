09b3e0778735ce9c421ceabaf281b237eaba650a
ruslantkachuk <Ruslan_Tkachuk@epam.com>
2019-11-07 19:58:12 +0200

    Added icons

--- main.ts
+++ main.ts
@@ -66,7 +66,7 @@ try {
 
   app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+    // dock icons is clicked and there are no other windows open.
     if (win === null) {
       createWindow();
     }
